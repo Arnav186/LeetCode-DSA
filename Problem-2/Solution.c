2361c5fd72c833243036c47ff61b436e161b1af6
Arnav Nawade <arnavnawade@gmail.com>
2026-02-19 01:02:08 +0530

    Add two numbers

---
+++ Problem-2/Solution.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+
+struct ListNode* newNode(int val) {
+    struct ListNode* node = (struct ListNode*)malloc(sizeof(struct ListNode));
+    node->val = val;
+    node->next = NULL;
+    return node;
+}
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode dummy;
+    struct ListNode* curr = &dummy;
+    dummy.next = NULL;
+
+    int carry = 0;
+
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+        int sum = carry;
+
+        if (l1 != NULL) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if (l2 != NULL) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        carry = sum / 10;
+        sum = sum % 10;
+
+        curr->next = newNode(sum);
+        curr = curr->next;
+    }
+
+    return dummy.next;
+}
