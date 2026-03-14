28de076384d00ac272ee4dd329e715b6854b2b53
Arnav Nawade <arnavnawade@gmail.com>
2026-03-14 15:21:38 +0530

    Range Sum Query - Immutable

---
+++ Problem-303/Solution.java
@@ -0,0 +1,22 @@
+class NumArray {
+    
+    int ans[];
+
+    public NumArray(int[] nums) {
+        ans = new int[nums.length + 1];
+
+        for (int i = 0; i < nums.length; i++){
+            ans[i+1] = ans[i] + nums[i];
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+        return ans[right+1] - ans[left];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
