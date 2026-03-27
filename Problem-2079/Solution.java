11e0ac6cbe3822d8c8463a312ada11c07cfe84f2
Arnav Nawade <arnavnawade@gmail.com>
2026-03-27 20:55:00 +0530

    Watering Plants

---
+++ Problem-2079/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int wateringPlants(int[] plants, int capacity) {
+        int steps = 0;
+        int water = capacity;
+
+        for ( int i = 0; i < plants.length; i++){
+            if (water < plants[i]){
+                steps = steps + (i * 2);
+                water = capacity;
+            }
+            water = water - plants[i];
+            steps++;
+        }
+        return steps;
+    }
+}
