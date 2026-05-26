88f4817ff8fea48e0cb2e9a5278fd631430f12ce
Arnav Nawade <arnavnawade@gmail.com>
2026-05-26 22:12:44 +0530

    Next Greater Element I

---
+++ Problem-496/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            int target = nums1[i];
+            int greater = -1;
+
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums2[j] == target) {
+                    for (int k = j + 1; k < nums2.length; k++) {
+                        if (nums2[k] > target) {
+                            greater = nums2[k];
+                            break;
+                        }
+                    }
+                }
+            }
+            ans[i] = greater;
+        }
+        return ans;
+    }
+}
