38428dac43dc321f85a4eec87e41edbc6845c70a
Arnav Nawade <arnavnawade@gmail.com>
2026-06-30 20:11:55 +0530

    Sum of All Subset XOR Totals

---
+++ Problem-1863/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int ans = 0;
+        for(int i = 0; i < nums.length; i++){
+            ans = ans | nums[i];
+        }
+        return ans << (nums.length-1);
+    }
+}
