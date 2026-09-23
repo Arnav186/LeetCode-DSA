bdd18311f020ba921c2bfbcf909f293b570da42d
Arnav Nawade <arnavnawade@gmail.com>
2026-09-23 21:03:27 +0530

    Minimum Operations to Reduce X to Zero

---
+++ Problem-1658/Solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        int total = 0;
+
+        for(int num : nums){
+            total += num;
+        }
+
+        int target = total - x;
+
+        if (target < 0){
+            return -1;
+        }
+        if (target == 0){
+            return nums.length;
+        }
+
+        int left = 0;
+        int sum = 0;
+        int max = -1;
+
+        for(int right = 0; right < nums.length; right++){
+            sum += nums[right];
+
+            while(sum > target){
+                sum -= nums[left++];
+            }
+
+            if(sum == target){
+                max = Math.max(max, right - left + 1);
+            }
+        }
+        if(max == -1){
+            return -1;
+        } else {
+            return (nums.length - max);
+        }
+    }
+}
